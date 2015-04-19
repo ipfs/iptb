9b53f264e141f08d3aedd74f921f70896492ddee
Jeromy <jeromyj@gmail.com>
2015-04-19 01:06:04 -0700

    add license readme and helptext

--- main.go
+++ main.go
@@ -214,8 +214,30 @@ func IpfsShell(n int) error {
 	return syscall.Exec(shell, []string{shell}, nenvs)
 }
 
+var helptext = `
+Ipfs Testbed
+
+commands:
+	init -n=[number of nodes]
+	    creates and initializes 'n' repos
+	start 
+	    starts up all testbed nodes
+	stop 
+	    kills all testbed nodes
+	restart
+	    kills, then restarts all testbed nodes
+
+	shell [n]
+	    execs your shell with environment variables set as follows:
+	        IPFS_PATH - set to testbed node n's IPFS_PATH
+	        NODE[x] - set to the peer ID of node x
+`
+
 func main() {
 	count := flag.Int("n", 0, "number of ipfs nodes to initialize")
+	flag.Usage = func() {
+		fmt.Println(helptext)
+	}
 
 	flag.Parse()
 
