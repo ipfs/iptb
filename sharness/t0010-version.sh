aa5ac12eac61cbf0563542ccef48580a0b29d330
Christian Couder <chriscool@tuxfamily.org>
2016-04-03 16:16:40 +0200

    t0010: add --help tests

--- sharness/t0010-version.sh
+++ sharness/t0010-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test_description="iptb --version"
+test_description="iptb --version and --help tests"
 
 . lib/test-lib.sh
 
@@ -14,6 +14,15 @@ test_expect_success "'iptb --version' works" '
 
 test_expect_success "'iptb --version' output looks good" '
 	egrep "^iptb version [0-9]+.[0-9]+.[0-9]+$" actual
+'
+
+test_expect_success "'iptb --help' works" '
+	iptb --help >actual
+'
+
+test_expect_success "'iptb --help' output looks good" '
+	grep "COMMANDS" actual &&
+	grep "USAGE" actual
 '
 
 test_done
