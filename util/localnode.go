6c0b872200a9ad917557727cd7dae64a5ecf6780
Jeromy <why@ipfs.io>
2016-06-11 15:33:47 -0700

    gx publish version 1.0.0

--- util/localnode.go
+++ util/localnode.go
@@ -14,8 +14,8 @@ import (
 	config "github.com/ipfs/go-ipfs/repo/config"
 	serial "github.com/ipfs/go-ipfs/repo/fsrepo/serialize"
 
-	manet "gx/ipfs/QmUBa4w6CbHJUMeGJPDiMEDWsM93xToK1fTnFXnrC8Hksw/go-multiaddr-net"
-	ma "gx/ipfs/QmYzDkkgAEmrcNzFCiYo6L1dTX4EAG1gZkbtdbd9trL4vd/go-multiaddr"
+	ma "github.com/jbenet/go-multiaddr"
+	manet "github.com/jbenet/go-multiaddr-net"
 )
 
 type LocalNode struct {
